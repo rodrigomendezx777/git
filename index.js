a2f1567f778c583ce6945e48b4c89053fe6ae6c7
Rodrigo Mendez <rodrigomendezx777@gmail.com>
2024-06-29 12:58:59 -0300

    aula sobre git e overview sobre html css e js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Ola mundo')
+const button = document.querySelector('#btn')
+const body = document.querySelector('body')
+const numberWrapper = document.createElement('div')
+
+let numero = 0
+
+
+function transformarBodyEmVermelho() {
+    body.classList.toggle('ativo')
+    body.appendChild(numberWrapper)
+    numberWrapper.innerHTML = numero
+    ++numero
+}
+
+
+button.addEventListener('click', () => transformarBodyEmVermelho())
+
+// button.addEventListener('click', () => {
+//     body.style.background = 'red'
+// })
+
